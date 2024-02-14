a8ba211ae99399fcaeb93ebfbd0043684777bbc6
Julian Mendez <julian@hopis.co>
2024-02-14 09:22:42 -0800

    try average interpolation

--- lib/opentopodata/backend.py
+++ lib/opentopodata/backend.py
@@ -108,7 +108,7 @@ def _validate_points_lie_within_raster(xs, ys, lats, lons, bounds, res):
     oob_indices.update(np.nonzero(~y_in_bounds)[0])
     return sorted([])#oob_indices)
 
-def _get_elevation_from_path(latitudes, longitudes, path, interpolation=Resampling.nearest):
+def _get_elevation_from_path(latitudes, longitudes, path, interpolation=Resampling.cubic):
     """Read values at locations in a raster.
 
     Args:
