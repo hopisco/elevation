f0ceaf6949d55fa420dfd277d17452ba182b25be
Julian Mendez <julian@hopis.co>
2024-02-14 08:32:40 -0800

    improve testing

--- testscript.py
+++ testscript.py
@@ -1,5 +1,6 @@
 import lib.opentopodata  as opentopodata
 
+opentopodata.SELECT_METHOD = 'OLD'
 r = opentopodata.getElevation('oxscF`hiiVIl@Ed@ERQf@CP?NB^ANEPGJYd@ILENENCPAP?TFr@?XAbA?TCXIj@AZAT@dA?`@@VBNDLNZVb@Tb@`@l@PXHV@JCNi@zAk@t@EBE?EC_@YGEK@E@GFMRKVSZ[`@Yn@Y~@I\E`@A^?bACVCRQ`@_AfBe@dA_@l@w@|AS^}CpDMRIVIVSfAM\IP]f@Ob@Nl@Td@Pl@B`@@h@F^ZbDAJCHEDOD}@`@UH_@Ju@XKDi@f@c@V_Bp@Bf@HLZT^`ANh@Rp@dGhInAjBPRDJ@FDJz@jAb@h@HFHDTTV^@J~@dALHFLPVKJk@h@CBg@`@oBdBw@n@gFrEaEdDiGhFyGvFuOpMmTzQuFvE}GxFDNBFEDk@b@c@^[TCBMJs@n@[XWROTQJYVOPi@f@MLg@\WR_@\kA|@OJQLi@h@gBrA[Vc@^GFOJi@^WPCJ@PGFBDFTRd@KHHPBT@d@Cd@I\IZS\OLGFRnAODCEACCACAC?E@CBABAB?D?D?@?A?E?E@C@CBCDAB?B@B@@BBDNESoAFGNMR]H[H]Be@Ae@CUIQJISe@GUCEFGAQBKVQh@_@NKFGb@_@ZWfBsAh@i@PMNKjA}@^]VSf@]LMh@g@NQXWPKNUVSZYr@o@LKBCZUb@_@j@c@DECGEO|GyFtFwElT{QtOqMxGwFhGiF`EeDfFsEv@o@nBeBf@a@BCj@i@JKQWGMMI_AeAAKW_@UUIEIGc@i@{@kAEKAGEKQSoAkBeGiISq@Oi@_@aA[UIMCg@~Aq@b@Wh@g@JEt@Y^KTI|@a@NEDEBI@K[cDG_@Ai@Ca@Qm@Ue@Om@Nc@\g@HQL]RgAHWHWLS|CqDR_@v@}A^m@d@eA~@gBPa@BSBW?cA@_@Da@H]X_AXo@Za@R[JWLSFGDAJAFD^XDBD?DCj@u@h@{ABOAKIWQYa@m@Uc@Wc@O[EMCOAW?a@AeA@U@[Hk@BY?U@cA?YGs@?U@QBQDODOHMXe@FKDQ@OC_@?OBQPg@DSDe@Hm@')
 print(r)
 
